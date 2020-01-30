5d45749511e7d1c390425620156d3f9653af055e
abickford <atb5498@gmail.com>
2020-01-29 18:58:18 -0600

    created table values

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ PRIMARY KEY(id)
 CREATE TABLE role(
 id INT AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
-salary DECIMAL(10,2) NOT NULL,
+salary INT NOT NULL,
 department_id INT NOT NULL,
 PRIMARY KEY(id)
 );
@@ -23,6 +23,15 @@ id INT AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT NULL,
 PRIMARY KEY(id)
 );
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Salesperson", 80000, 2), ("Lead Engineer", 150000, 3), ("Software Engineer", 120000, 4), ("Accountant", 125000, 5), ("Assistant Accountant
+", 90000, 6), ("Legal Team Lead", 250000, 7), ("Lawyer", 190000, 8);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1), ("Mike", "Chan", 2), ("Ashley", "Rodriguez", 3), ("Kevin", "Tupik", 4), ("Malia", "Brown", 5), ("Sarah", "Lourd", 6), ("Tom", "Allen", 7), ("Christian", "Eckenrode", 8);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 const express = require("express");
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
