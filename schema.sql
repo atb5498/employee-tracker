c6d0c282483d4f2f823e3850701e96e918ae9eb3
abickford <atb5498@gmail.com>
2020-01-29 22:03:04 -0600

    created .env, joined all 3 tables, and connected server to database

--- schema.sql
+++ schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_tacker_db;
 
 USE employee_tacker_db;
 
-CREATE TABLE department(
+CREATE TABLE departments(
 id INT AUTO_INCREMENT NOT NULL,
-name VARCHAR(30) NOT NULL,
+department VARCHAR(30) NOT NULL,
 PRIMARY KEY(id)
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
 id INT AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
 salary INT NOT NULL,
@@ -18,7 +18,7 @@ department_id INT NOT NULL,
 PRIMARY KEY(id)
 );
 
-CREATE TABLE employee(
+CREATE TABLE employees(
 id INT AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
@@ -26,12 +26,12 @@ role_id INT NOT NULL,
 PRIMARY KEY(id)
 );
 
-INSERT INTO department (name)
+INSERT INTO departments (department)
 VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 100000, 1), ("Salesperson", 80000, 2), ("Lead Engineer", 150000, 3), ("Software Engineer", 120000, 4), ("Accountant", 125000, 5), ("Assistant Accountant
-", 90000, 6), ("Legal Team Lead", 250000, 7), ("Lawyer", 190000, 8);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Salesperson", 80000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Accountant", 125000, 3), ("Assistant Accountant
+", 90000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id)
+INSERT INTO employees (first_name, last_name, role_id)
 VALUES ("John", "Doe", 1), ("Mike", "Chan", 2), ("Ashley", "Rodriguez", 3), ("Kevin", "Tupik", 4), ("Malia", "Brown", 5), ("Sarah", "Lourd", 6), ("Tom", "Allen", 7), ("Christian", "Eckenrode", 8);
--- server.js
+++ server.js
@@ -2,3 +2,16 @@ const express = require("express");
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    database: "employee_tracker_db",
+    password: process.env.DB_Password
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
