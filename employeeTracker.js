c742f902e289d281529e22fca8044965b02c9e12
abickford <atb5498@gmail.com>
2020-01-30 18:48:31 -0600

    created employees by department prompts and queries

--- employeeTracker.js
+++ employeeTracker.js
@@ -27,12 +27,63 @@ function promptUser() {
     ]).then(function (response) {
         if (response.menu === "View All Employees") {
             viewAllEmployees();
+        } else if (response.menu === "View Employees By Department") {
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "department",
+                    message: "Choose a department.",
+                    choices: ["Sales", "Engineering", "Finance", "Legal"]
+                }
+            ]).then(function (response) {
+                if (response.department === "Sales") {
+                    viewSalesEmployees();
+                } else if (response.department === "Engineering") {
+                    viewEngineeringEmployees();
+                } else if (response.department === "Finance") {
+                    viewFinanceEmployees();
+                } else {
+                    viewLegalEmployees();
+                }
+            })
         }
     });
 }
 
 function viewAllEmployees() {
     connection.query("SELECT employees.id, employees.first_name, employees.last_name, roles.salary, roles.title, departments.department FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id;", function (error, response) {
+        if (error) throw error;
+        console.table(response);
+        promptUser();
+    });
+}
+
+function viewSalesEmployees() {
+    connection.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.salary, roles.title, departments.department FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id WHERE departments.department = "Sales";`, function (error, response) {
+        if (error) throw error;
+        console.table(response);
+        promptUser();
+    });
+}
+
+function viewEngineeringEmployees() {
+    connection.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.salary, roles.title, departments.department FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id WHERE departments.department = "Engineering";`, function (error, response) {
+        if (error) throw error;
+        console.table(response);
+        promptUser();
+    });
+}
+
+function viewFinanceEmployees() {
+    connection.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.salary, roles.title, departments.department FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id WHERE departments.department = "Finance";`, function (error, response) {
+        if (error) throw error;
+        console.table(response);
+        promptUser();
+    });
+}
+
+function viewLegalEmployees() {
+    connection.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.salary, roles.title, departments.department FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id WHERE departments.department = "Legal";`, function (error, response) {
         if (error) throw error;
         console.table(response);
         promptUser();
